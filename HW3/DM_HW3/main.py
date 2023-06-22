f34e77f29009e09c71b046c9bc3d4fa0195e7b23
mohamadch91 <mohamadchoupan80@gmail.com>
2023-06-22 16:05:20 +0330

    feat: add jupyter file

--- HW3/DM_HW3/main.py
+++ HW3/DM_HW3/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
+import random
 def read_csv(file)->pd.DataFrame:
     # read csv file from pandas
     df = pd.read_csv(file)
@@ -18,7 +19,7 @@ def eucilidean_distance(x,y)->int:
     # calculate euclidiean distance of two point 
     return math.sqrt(sum( (x[i]-y[i])**2 for i in range(2)))
     
-def similarity_vector (dataset,type)-> np.array :
+def similarity_vector (dataset,type=None)-> np.array :
     lentgh = len(dataset)
     sim_vector=np.zeros([lentgh,lentgh] , dtype=float)
     for i in range(lentgh):
@@ -28,10 +29,46 @@ def similarity_vector (dataset,type)-> np.array :
             else:
                 sim_vector[i][j]=eucilidean_distance(dataset[i],dataset[j])
     return sim_vector    
+
+def should_stop(data_set,old,new,iteration):
+    if (old is None):
+        return False
+    if iteration > 400 :
+        return True
+    sum=0
+    for i,j in zip(old,new):
+        distance= eucilidean_distance(data_set[i],data_set[j])
+        sum += distance
+    if distance < 1:
+        return True
     
+    return False
+    
+
+
+def get_random_centroids(lentgh,k):
+    centroids=[]
+    for i in range(k):
+        centroids.append(random.randint(0,lentgh))
+    return centroids
+    
+def kmeans (data_set,k):
+    centroids=get_random_centroids(len(data_set),4)
+    iteration = 0
+    old_centroid= 
+    while not should_stop (data_set,old_centroid,centroids,iteration):
+        print('salam')
+        old_centroid = centroids
+        iteration +=1
+None
     
 df=read_csv('data.csv')
-sim_vec=similarity_vector(df,"cosine")
-print(sim_vec[60][60])
+sim_vec_cosine=similarity_vector(df,"cosine")
+sim_vec_euc=similarity_vector(df)
+
+centroids=get_random_centroids(len(df),4)
+kmeans(df,4)
+print(centroids)
+print(sim_vec_euc[60][60])
 
     
